d6c784f65ad1e74f4ab5fc0877bbb8feb111b58d
quentin-chirol <157401619+quentin-chirol@users.noreply.github.com>
2024-01-29 21:45:01 +0100

    Add files via upload

---
+++ chek.py
@@ -0,0 +1,37 @@
+from tools.analyse import *
+from tools.web_selenium import *
+import numpy as np
+import sys
+
+web = selenium("https://www.binance.com/fr/support/announcement/nouveaux-listing-de-cryptomonnaies?c=48",10)
+
+cookie_element  = "//button[@id='onetrust-reject-all-handler']"
+web.click_items(cookie_element)
+
+annonce_link = "//div[2]/div[2]/div[2]/section/div/div/div[3]/a/div"
+annonce = web.click_items(annonce_link)
+
+web.end()
+
+bool = np.load("numpy/bool.npy")
+if bool == True:
+    print("attente d'une execution de paire")
+    sys.exit()
+
+var = analyse(annonce)
+
+if var.check_paire(["introduction", "listera"]) == True:
+    paire = var.paire()
+else:
+    print("aucune paire detectée")
+    sys.exit()
+
+previous_paire  =  np.load("numpy/paire.npy")
+if previous_paire == paire:
+    print("paire déja executer")
+    sys.exit()
+
+
+data_paire = np.array(paire)
+np.save("numpy/paire.npy", data_paire)
+np.save("numpy/bool.npy", True)
---
+++ execute.py
@@ -0,0 +1,40 @@
+import numpy as np
+import sys
+import ccxt
+from datetime import datetime
+from datetime import timedelta 
+import json
+
+loaded_bool= np.load("numpy/bool.npy")
+if loaded_bool  ==  False:
+    print("attente de trouver de paire")
+    sys.exit()
+
+f = open('config.json',)
+mdp = json.load(f)
+f.close()
+
+audenthifiation =  {
+    'apiKey': mdp["API"],
+    'secret': mdp["API_secret"],
+    'options': {
+        'defaultType': 'spot',  #margin, spot, future, per, ect
+    }
+}
+binance = ccxt.binance(audenthifiation)
+
+loaded_paire  =  np.load("numpy/paire.npy")
+current_price  = (binance.fetch_ticker(loaded_paire[0]["ask"]) + binance.fetch_ticker(loaded_paire[0]["bid"])) /2
+amount_usdt  =  10
+amount = amount_usdt / current_price
+order = binance.create_order(loaded_paire[0], "market", "buy", amount)
+np.save("numpy/bool.npy", False)
+entry_price = order['price']
+order_size = order['amount']
+
+target_5000 =  entry_price  +  entry_price*50  
+binance.create_order(loaded_paire[0], "limit", "sell", amount, target_5000)
+
+
+
+
