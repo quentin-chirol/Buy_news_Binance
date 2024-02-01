850156ab7ce41bdcfe08a8a262c009e6c4d32daa
quentin-chirol <157401619+quentin-chirol@users.noreply.github.com>
2024-02-01 12:34:59 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -23,10 +23,10 @@ binance = ccxt.binance(audenthifiation)
 
 loaded_paire  =  np.load("/home/you_server_name/Buy_news_Binance/numpy/paire.npy")
 current_price = (binance.fetch_ticker(loaded_paire[0])["ask"] + binance.fetch_ticker(loaded_paire[0])["bid"]) / 2
-amount_usdt  =  10
+amount_usdt  =  100
 amount = amount_usdt / current_price
 order = binance.create_order(loaded_paire[0], "market", "buy", amount)
-np.save("numpy/bool.npy", False)
+np.save("/home/you_server_name/Buy_news_Binance/numpy/bool.npy", False)
 #entry_price = order['price']
 #order_size = order['amount']
 
