eadc98a1cfb9ab9950f81af469e1325825f92f11
quentin-chirol <157401619+quentin-chirol@users.noreply.github.com>
2024-02-03 19:22:02 +0000

    Update execute.py

--- execute.py
+++ execute.py
@@ -3,11 +3,6 @@ import sys
 import ccxt
 import json
 
-loaded_bool= np.load("/home/you_server_name/Buy_news_Binance/numpy/bool.npy")
-if loaded_bool  ==  False:
-    print("attente de trouver de paire")
-    sys.exit()
-
 f = open('/home/you_server_name/Buy_news_Binance/config.json',)
 mdp = json.load(f)
 f.close()
@@ -21,18 +16,27 @@ audenthifiation =  {
 }
 binance = ccxt.binance(audenthifiation)
 
+loaded_bool= np.load("/home/you_server_name/Buy_news_Binance/numpy/bool.npy")
 loaded_paire  =  np.load("/home/you_server_name/Buy_news_Binance/numpy/paire.npy")
-current_price = (binance.fetch_ticker(loaded_paire[0])["ask"] + binance.fetch_ticker(loaded_paire[0])["bid"]) / 2
-amount_usdt  =  100
-amount = amount_usdt / current_price
-order = binance.create_order(loaded_paire[0], "market", "buy", amount)
-np.save("/home/you_server_name/Buy_news_Binance/numpy/bool.npy", False)
-#entry_price = order['price']
-#order_size = order['amount']
 
-# creat limit order
-#target_5000 =  entry_price  +  entry_price*50  
-#binance.create_order(loaded_paire[0], "limit", "sell", amount, target_5000)
+if loaded_bool  ==  True:
+    #market order
+    current_price = (binance.fetch_ticker(loaded_paire[0])["ask"] + binance.fetch_ticker(loaded_paire[0])["bid"]) / 2
+    amount_usdt  =  100
+    amount = amount_usdt / current_price
+    order = binance.create_order(loaded_paire[0], "market", "buy", amount)
+    np.save("/home/you_server_name/Buy_news_Binance/numpy/bool.npy", False)
+
+    #limit order
+    entry_price = order['price']
+    order_size = order['amount']
+    target_1000 =  entry_price  +  entry_price*10
+    binance.create_order(loaded_paire[0], "limit", "sell", order_size, target_1000)
+    
+else:
+    print("attente de trouver de paire")
+    sys.exit()
+
 
 
 
