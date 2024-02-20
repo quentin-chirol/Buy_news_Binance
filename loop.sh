27a855acd0e488bdaf3d34dd0f9935162cb9c26c
quentin-chirol <157401619+quentin-chirol@users.noreply.github.com>
2024-02-20 15:47:04 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -2,7 +2,7 @@ API_KEY="x"
 SECRET_KEY="x"
 
 # Set up the request:
-BASE_URL="https://api.binance.com"
+BASE_URL="https://api.binance.us"
 API_METHOD="POST"
 API_CALL="api/v3/order"
 SYMBOL=`jq -r '.paire' /path/data.json`
